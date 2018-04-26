801487c7f6702a39f98ba4f40e85767455e9f429
Victor <victorlaucas@gmail.com>
2018-04-25 23:08:08 -0600

    Created birthdayForm component and added datepicker to package.json

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import BirthdayForm from './birthdayForm';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <BirthdayForm />
       </div>
     );
   }
