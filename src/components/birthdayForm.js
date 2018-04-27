b2aedd022c94238a186c89e26b3c976e1e6acc6f
Victor <victorlaucas@gmail.com>
2018-04-26 23:14:58 -0600

    Added clock component

--- src/components/birthdayForm.js
+++ src/components/birthdayForm.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import DatePicker from 'react-datepicker';
 import moment from 'moment';
+import Clock from './Clock';
 
 class BirthdayForm extends Component {
 
@@ -8,7 +9,7 @@ class BirthdayForm extends Component {
         super(props);
 
         this.handleChange = this.handleChange.bind(this);
-        this.handleGenerate = this.handleGenerate.bind(this);
+        this.handleGenerate = this.handleGenerate.bind(this)
 
         this.state = {
             startDate: moment(),
@@ -18,28 +19,39 @@ class BirthdayForm extends Component {
     }
 
     handleChange(date) {
-
         this.setState({
             startDate: date
-        })
-
+        });
     }
+
     handleGenerate() {
-      this.setState({
-        formCompleted: true
-      })
+        this.setState({
+            formCompleted: true
+        })
     }
-
 
     render() {
         return (
             <div>
-                <h1>Birthday Form Component!</h1>
-                <DatePicker
-                    selected={this.state.startDate}
-                    onChange={this.handleChange}
-                />
-                <a onClick={this.handleGenerate}>Generate Countdown</a>
+            {
+                this.state.formCompleted ?
+                
+                    <div>
+                        <Clock birthdayFormState={this.state}/>
+                    </div>
+                :
+                    <div>
+                        <h1>Birthday Form Component!</h1>
+                        <DatePicker
+                            selected={this.state.startDate}
+                            onChange={this.handleChange}
+                        />
+                        <a onClick={this.handleGenerate}>Generate Countdown</a>
+                    </div>
+
+            }
+                
+
             </div>
         )
     }
---
+++ src/components/clock.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+
+class Clock extends Component {
+
+
+    constructor(props) {
+        super(props)
+
+
+        this.state = {
+            timeRemaining: {}
+        }
+
+    }
+
+    render() {  
+        const data = this.state.timeRemaining
+
+        return (
+            <div>
+                <div>DAYS {data.days}</div>
+                <div>HRS {data.hours}</div>
+                <div>MINS {data.minutes}</div>
+                <div>SECS {data.seconds}</div>
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        this.birthday = this.props.birthdayFormState.startDate.toString();
+        alert(this.birthday);
+    }
+}
+
+export default Clock;
