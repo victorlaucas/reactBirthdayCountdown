57b2467ac6e2d6047b8fc8f3f0e0f537ba2e92eb
Victor <victorlaucas@gmail.com>
2018-04-29 22:14:09 -0600

    Added happy birthday message styles

--- src/components/clock.js
+++ src/components/clock.js
@@ -101,7 +101,11 @@ class Clock extends Component {
             <div className="countdown">
             {
                 this.state.timeRemaining == 0 ?
-                    <h1>Happy Birthday!</h1>
+                    <div className="message-container">
+                        <p className="message-container__title">Countdown Complete</p>
+                        <p className="message-container__message">HAPPY BIRTHDAY!</p>
+                    </div>
+                   
                     :
                     <div>
                         <div>
